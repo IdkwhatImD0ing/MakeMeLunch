1de1ac464bee8e9158ed66f852f55bc15ee2b3cc
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2022-07-15 21:04:01 -0700

    Rudimentary auth

---
+++ makemelunch/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZP-gvy7yEPKVImW7Ho047jeq6dvuX9Qc",
+  authDomain: "makemelunch-d8d20.firebaseapp.com",
+  databaseURL: "https://makemelunch-d8d20-default-rtdb.firebaseio.com",
+  projectId: "makemelunch-d8d20",
+  storageBucket: "makemelunch-d8d20.appspot.com",
+  messagingSenderId: "947794987440",
+  appId: "1:947794987440:web:738d022fae02723b836e75",
+  measurementId: "G-0Q0DBY0N4E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ makemelunch/pages/login.js
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import styles from "../styles/Home.module.css";
+import { useAuth } from "../context/UserAuthContext";
+import { setErrorMessage } from "../context/setErrorMessage";
+
+const Login = () => {
+  const router = useRouter();
+  const auth = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (event, email, password) => {
+    event.preventDefault();
+
+    auth
+      .signIn(email, password)
+      .then(() => {
+        // do something after signing in. For example, router.push("/");
+        router.push("/");
+      })
+      .catch((error) => {
+        let { title, description } = setErrorMessage(error);
+        // do something with error title and description here
+        alert(title + ": " + description);
+      });
+  };
+
+  // if there is no signed in user
+  if (!auth.user) {
+    return (
+      <div className={styles.container}>
+        
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>Login</h1>
+          <br />
+          <form onSubmit={(event) => signIn(event, email, password)}>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <Link href="/">&larr; Go back</Link>
+        </main>
+      </div>
+    );
+  }
+};
+
+export default Login;

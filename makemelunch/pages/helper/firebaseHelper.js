870a7e643ffc7782c4b0d296dd94ef1b12d5b66d
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2022-07-16 10:49:42 -0700

    Added helper function

--- makemelunch/pages/helper/firebaseHelper.js
+++ makemelunch/pages/helper/firebaseHelper.js
@@ -13,6 +13,7 @@ function addIngredient(ingredient) {
   let docRef = colRef.doc("ingredients");
 
   setDoc(docRef, { ingredient: 1 }, { merge: true });
+  return;
 }
 
 function deleteIngredient(ingredient) {
@@ -24,4 +25,26 @@ function deleteIngredient(ingredient) {
   let docRef = colRef.doc("ingredients");
 
   updateDoc(docRef, { ingredient: deleteField() });
+  return;
+}
+
+function getAllIngredients() {
+  const auth = userAuth();
+  const router = useRouter();
+
+  let userId = auth.currentUser.uid;
+  let colRef = database.collection(userId);
+  let docRef = colRef.doc("ingredients");
+
+  let fields = Object.keys(docRef.data());
+  if (fields.length == 0) {
+    return "error";
+  }
+  let string = fields[0];
+  for (let i = 1; i < fields.length; i++) {
+    string += ",";
+    string += fields[i];
+  }
+
+  return fields;
 }

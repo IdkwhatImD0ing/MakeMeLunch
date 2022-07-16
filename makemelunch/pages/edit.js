234d7d24a71c50e9c4c87fadf841a09084eb64e2
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2022-07-16 12:00:38 -0700

    Added homepage linking

--- makemelunch/pages/edit.js
+++ makemelunch/pages/edit.js
@@ -5,7 +5,7 @@ export default function Edit () {
                 <a>MakeMyLunch</a>
                 <nav>
                     <ul>
-                        <li><a href="/homepage">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="/edit">Add/Remove Ingredients</a></li>
                         <li><a href="/getrecipes">Get Recipes</a></li>
                         <li><a href="#">Sign Out</a></li>

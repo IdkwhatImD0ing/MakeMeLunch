98350f60bb60b5da9f95105a8d614fc5cdd09345
shareysanr <shareysanr1@gmail.com>
2022-07-16 02:28:07 -0400

    Adding home page and links

---
+++ makemelunch/pages/getrecipes.js
@@ -0,0 +1,18 @@
+export default function GetRecipes () {
+    return (
+        <>
+            <header>
+                <a>MakeMyLunch</a>
+                <nav>
+                    <ul>
+                        <li><a href="/homepage">Home</a></li>
+                        <li><a href="/edit">Add/Remove Ingredients</a></li>
+                        <li><a href="/getrecipes">Get Recipes</a></li>
+                        <li><a href="#">Sign Out</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <h1>Get Recipes</h1>
+        </>
+    )
+}

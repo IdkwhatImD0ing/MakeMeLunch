411905bb9be222079d42337ac189521b5a6d5d3b
Elissa Yang <eyang.zeta@gmail.com>
2022-07-16 18:37:15 -0700

    buttons

--- makemelunch/pages/index.js
+++ makemelunch/pages/index.js
@@ -30,39 +30,56 @@ export default function Home() {
       color: "green",
       padding: "10px",
       fontFamily: "Sans-Serif",
-      alignItems: "center"
+      textAlign: "center",
     };
 
     const editStyle = {
       padding: "10px",
-      alignItems: "center"
+      textAlign: "center"
     }
+
+    const buttonStyle = {
+      padding: "10px",
+      textAlign: "center",
+      color: "blue"
+    }
+    
     return (
       <>
-        <h1 style={headStyle}>MakeMyLunch</h1>
-        <h2>
-        <a href="/">Home</a>
-          <Link href="/">
-            <a onClick={() => handleClick()}> Log Out</a>
-          </Link>
-        </h2>
         <Container fluid style={editStyle}>
-          <Row sm={8}>
-            <Col sm={5}><a href="/ingredient/viewingredients">View Ingredients</a>
-              <a href="/getrecipes">Get Recipes</a></Col>
-          </Row>
-
           <Row>
-            <Col><a href="/ingredient/addIng">Add Ingredients</a></Col>
+            <h1 style={headStyle}>MakeMyLunch</h1>
+
+            <Button>
+
+              <a href="/">Back to Home</a>
+            </Button>
+            <Button>
+              <Link href="/">
+                <a onClick={() => handleClick()}> Log Out</a>
+
+              </Link>
+            </Button>
+          </Row>
+          <Row>
+            <Col><a href="/ingredient/viewingredients">View Ingredients</a>
+            </Col>
+            <Col>
+              <a href="/ingredient/addIng">Add Ingredients</a></Col>
+
             <Col>
               <a href="/ingredient/deleteIng">Remove Ingredients</a>
             </Col>
-
           </Row>
+          <Row>
+            <Col>
+              <Button style = {buttonStyle}>
+                <a href="/getrecipes">Get Recipes</a></Button></Col>
+          
 
-        </Container>
-        
 
+        </Row>
+      </Container>
       </>
     );
   }
@@ -110,7 +127,6 @@ export default function Home() {
             justify-content: center;
             align-items: center;
           }
-
           main {
             padding: 5rem 0;
             flex: 1;
@@ -119,7 +135,6 @@ export default function Home() {
             justify-content: center;
             align-items: center;
           }
-
           footer {
             width: 100%;
             height: 100px;
@@ -128,50 +143,41 @@ export default function Home() {
             justify-content: center;
             align-items: center;
           }
-
           footer img {
             margin-left: 0.5rem;
           }
-
           footer a {
             display: flex;
             justify-content: center;
             align-items: center;
           }
-
           a {
             color: inherit;
             text-decoration: none;
           }
-
           .title a {
             text-align: center;
             color: #0070f3;
             text-decoration: none;
           }
-
           .title a:hover,
           .title a:focus,
           .title a:active {
             text-decoration: underline;
           }
-
           .title {
             margin: 0;
             line-height: 1.15;
             font-size: 4rem;
           }
-
           .title,
           .description {
             text-align: center;
           }
-
           .description {
             line-height: 1.5;
             font-size: 1.5rem;
           }
-
           code {
             background: #fafafa;
             border-radius: 5px;
@@ -180,17 +186,14 @@ export default function Home() {
             font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
               DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
           }
-
           .grid {
             display: flex;
             align-items: center;
             justify-content: center;
             flex-wrap: wrap;
-
             max-width: 800px;
             margin-top: 3rem;
           }
-
           .card {
             margin: 1rem;
             flex-basis: 45%;
@@ -202,29 +205,24 @@ export default function Home() {
             border-radius: 10px;
             transition: color 0.15s ease, border-color 0.15s ease;
           }
-
           .card:hover,
           .card:focus,
           .card:active {
             color: #0070f3;
             border-color: #0070f3;
           }
-
           .card h3 {
             margin: 0 0 1rem 0;
             font-size: 1.5rem;
           }
-
           .card p {
             margin: 0;
             font-size: 1.25rem;
             line-height: 1.5;
           }
-
           .logo {
             height: 1em;
           }
-
           @media (max-width: 600px) {
             .grid {
               width: 100%;
@@ -242,7 +240,6 @@ export default function Home() {
               Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
               sans-serif;
           }
-
           * {
             box-sizing: border-box;
           }

5a0a517511dfc391da67b19aa582a0b8a97df80e
billzhangsc <billzhangsc@gmail.com>
2022-07-25 11:26:00 -0700

    Changed background images and added footer to every page

--- makemelunch/pages/ingredient/addIng.js
+++ makemelunch/pages/ingredient/addIng.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { useAuth } from "../../context/UserAuthContext";
 import { addIngredient, ingChanged } from "../../helper/firebaseHelper";
 import MainAppBar from "../components/mainappbar";
-import ProjectQuestion from "../components/projectquestion";
+import StickyProductQuestion from "../components/stickyproductquestion";
 import { Button, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { TextField, Box } from "@mui/material";
@@ -26,6 +26,9 @@ const theme = createTheme({
   },
 });
 
+const url =
+  "https://images.unsplash.com/photo-1490735891913-40897cdaafd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+
 const Add = () => {
   const router = useRouter();
   const auth = useAuth();
@@ -44,49 +47,63 @@ const Add = () => {
         <title>Add Ingredients</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MainAppBar />
-      <Container>
-        <Typography variant="h3" sx={{ my: 4, textAlign: "center" }}>
-          Add Ingredients
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={addIng}
-          noValidate
-          sx={{ mt: 1, textAlign: "center " }}
-        >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="ingredient"
-            label="Ingredient"
-            name="ingredient"
-            autoComplete="ingredient"
-            autoFocus
-          />
-          <Button
-            type="Submit"
-            fullWidth
-            variant="contained"
-            sx={{
-              mt: 3,
-              mb: 2,
-              backgroundColor: "green",
-              "&:hover": {
-                backgroundColor: "#fff",
-                color: "green",
-              },
-            }}
+
+      <Box
+        component="section"
+        sx={{
+          display: "flex",
+          overflow: "hidden",
+          backgroundImage: `url(${url})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        <MainAppBar />
+        <Container>
+          <Typography variant="h3" sx={{ my: 4, textAlign: "center" }}>
+            Add Ingredients
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={addIng}
+            noValidate
+            sx={{ mt: 1, textAlign: "center " }}
           >
-            Add
-          </Button>
-          <Link textAlign="center" href="javascript:history.back()">
-            &larr; Go back
-          </Link>
-        </Box>
-      </Container>
-      <ProjectQuestion />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="ingredient"
+              label="Ingredient"
+              name="ingredient"
+              autoComplete="ingredient"
+              autoFocus
+            />
+            <Button
+              type="Submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                backgroundColor: "green",
+                "&:hover": {
+                  backgroundColor: "#fff",
+                  color: "green",
+                },
+              }}
+            >
+              Add
+            </Button>
+            <Link textAlign="center" href="javascript:history.back()">
+              &larr; Go back
+            </Link>
+          </Box>
+        </Container>
+      </Box>
+      <StickyProductQuestion />
     </ThemeProvider>
   );
 };
--- makemelunch/pages/ingredient/searchByIng.jsx
+++ makemelunch/pages/ingredient/searchByIng.jsx
@@ -11,14 +11,15 @@ import {
   ingChanged,
 } from "../../helper/firebaseHelper";
 import { useRouter } from "next/router";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MainAppBar from "../components/mainappbar";
-import ProductQuestion from "../components/projectquestion";
+import ProductQuestion from "../components/productquestion";
+
+const url =
+  "https://images.unsplash.com/photo-1490735891913-40897cdaafd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
 
 function Recipes() {
   const auth = useAuth();
@@ -69,67 +70,104 @@ function Recipes() {
     router.push({ pathname: "/ingredient/showRecipe", query: { id: id } });
   };
 
-  return (
-    <>
-      <Head>
-        <title>Search With Ingredients</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <MainAppBar />
-      <main className={styles.main}>
-        <Container maxWidth="xl" sx={{ textAlign: "center" }}>
+  if (!recipeObject) {
+    return (
+      <>
+        <Head>
+          <title>Search With Ingredients</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Box
+          component="section"
+          sx={{
+            display: "flex",
+            overflow: "auto",
+            backgroundImage: `url(${url})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            flexDirection: "column",
+            height: "100vh",
+          }}
+        >
+          <MainAppBar />
+          <main className={styles.main}>
+            <Container maxWidth="xl" sx={{ textAlign: "center" }}>
+              <Typography variant="h3" sx={{ my: 4 }}>
+                Search based on your ingredients
+              </Typography>
+              <Box component="form" onSubmit={myrecipes} noValidate>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{
+                    mt: 3,
+                    mb: 2,
+                    backgroundColor: "green",
+                    "&:hover": {
+                      backgroundColor: "#fff",
+                      color: "green",
+                    },
+                  }}
+                >
+                  Search Now
+                </Button>
+              </Box>
+            </Container>
+            <br />
+            <Link href="javascript:history.back()">&larr; Go back</Link>
+            <br />
+          </main>
+        </Box>
+        <ProductQuestion />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <title>Search With Ingredients</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Box
+          component="section"
+          sx={{
+            display: "flex",
+            overflow: "hidden",
+            backgroundImage: `url(${url})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            flexDirection: "column",
+            textAlign: "center",
+          }}
+        >
+          <MainAppBar />
           <Typography variant="h3" sx={{ my: 4 }}>
-            Search based on your ingredinents
+            Search based on your ingredients
           </Typography>
-          <Box component="form" onSubmit={myrecipes} noValidate>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                backgroundColor: "green",
-                "&:hover": {
-                  backgroundColor: "#fff",
-                  color: "green",
-                },
-              }}
-            >
-              View Recipe
-            </Button>
-          </Box>
-        </Container>
-        <br />
-        <Link href="javascript:history.back()">&larr; Go back</Link>
-        <br />
-        {recipeObject && (
-          <div className={styles.main}>
-            {recipeObject.map((recipe, index) => (
-              <div id={index} key={recipe.id}>
-                <Container style={editStyle}>
-                  <h2>{recipe.title}</h2>
-                  <br />
-                  <img src={recipe.image} alt={recipe.title} />
-                  <br />
-                  <text>Id: {recipe.id}</text>
-                  <form onSubmit={(event) => viewRecipe(event, recipe.id)}>
-                    <button type="submit">More Info</button>
-                  </form>
-                  <br />
-                </Container>
-              </div>
-            ))}
-            {/* <div>
-              <span>Id: {recipeObject[0].id}</span>
-              <span>Name: {recipeObject[0].title}</span>
-              <img src={recipeObject[0].img} />
-            </div> */}
-          </div>
-        )}
-      </main>
-      <ProductQuestion />
-    </>
-  );
+          <Container>
+            <div className={styles.main}>
+              {recipeObject.map((recipe, index) => (
+                <div id={index} key={recipe.id}>
+                  <Container style={editStyle}>
+                    <h2>{recipe.title}</h2>
+                    <br />
+                    <img src={recipe.image} alt={recipe.title} />
+                    <br />
+                    <text>Id: {recipe.id}</text>
+                    <form onSubmit={(event) => viewRecipe(event, recipe.id)}>
+                      <button type="submit">More Info</button>
+                    </form>
+                    <br />
+                  </Container>
+                </div>
+              ))}
+            </div>
+          </Container>
+        </Box>
+        <ProductQuestion />
+      </>
+    );
+  }
 }
 
 export default Recipes;
